d675ee83efe8facb98d288c9c20f1b5706c017b3
nasirhussain <nasirhussain0@outlook.com>
2017-06-18 08:43:26 +0100

    resources folder

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,4 @@
+<hr>
+
+<p class="text-center">Copyright NasirHussain -- All Rights Reserved</p>
+
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.main')
+
+@section('title', '| View Post')
+
+@section('content')
+
+	<div class="row">
+		<div class="col-md-8">
+			<h1>{{ $post->title }}</h1>
+			
+			<p class="lead">{{ $post->body}}</p>
+		</div>
+
+		<div class="col-md-4">
+			<div class="well">
+				<dl class="dl-horizontal">
+					<label>URL:</label>
+					<p><a href=" {{ route('nasirhussain.single', $post->slug) }}"> {{route('nasirhussain.single', $post->slug)}}</a></p>
+				</dl>
+				<dl class="dl-horizontal">
+					<label>Created At:</label>
+					<p>{{ date('M j, Y h:ia', strtotime($post->created_at))}}</p>
+				</dl>
+				<dl class="dl-horizontal">
+					<label>Last Updated:</label>
+					<p>{{ date('M j, Y h:ia', strtotime($post ->updated_at))}}</p>
+				</dl>
+				<hr>
+				<div class="row">
+
+					<div class="col-sm-6">
+					{!! Html:: linkRoute('post.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block'))!!}
+					</div>
+
+					<div class="col-sm-6">
+					{!! Form::open(['route' => ['post.destroy', $post->id], 'method' => 'DELETE'])!!}
+
+					{!!Form::submit('Delete', ['class'=> 'btn btn-danger btn-block']) !!}
+
+					{!! Form::close() !!}
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-12">
+						{{Html::linkRoute('post.index', 'All Posts', [], ['class' => 'btn btn-default btn-block btn-h1-spacing'])}}
+					</div>
+				</div>
+			</div>	
+		</div>
+	</div>
+@endsection
